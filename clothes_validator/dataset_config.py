1a6b6f069105e4efee3f773f1d76de3c02811279
lukasb27 <lukasball@live.co.uk>
2025-03-18 13:32:24 +0000

    commit

---
+++ clothes_validator/dataset_config.py
@@ -0,0 +1,21 @@
+import kaggle
+import os
+import shutil
+
+def download_dataset(dataset: str="sunnykusawa/tshirts", path = "./data") -> None:
+    if os.path.exists(path):
+        print(f"Data exists at {path}, please provide an empty path.")
+    else:
+        print("Downloading dataset")
+        kaggle.api.dataset_download_files(dataset, path=path, unzip=True)
+        print("Download complete!")
+    
+def remove_dataset(path = "./data") -> None:
+    if os.path.exists(path):
+        shutil.rmtree(path)
+    else:
+        print("Path doesnt exist")
+    
+
+if __name__ == "__main__":
+    pass
---
+++ clothes_validator/dataset_label.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+def label_dataset():
+    root = Tk() 
+    a = Label(root, text ="Hello World") 
+    a.pack() 
+    
+    root.mainloop()
